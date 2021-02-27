ac0a03c0c6c5ee6d887b9ef3c780d33611757fb3
nitinkarthy <nitin.karthy@gmail.com>
2021-02-27 20:52:06 +0530

    Setting up project structure

---
+++ java/src/test/java/InitialTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class InitialTest {
+    @Test
+    void first() {
+        System.out.println("true = " + true);
+    }
+}
---
+++ js/src/first.test.js
@@ -0,0 +1,5 @@
+describe("First", () => {
+    it("should work", () => {
+        console.log("OK");
+    })
+})
