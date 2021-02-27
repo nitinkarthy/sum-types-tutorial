e67bb2a493814ef8ba3c8e39bd967ed3219b6e5f
nitinkarthy <nitin.karthy@gmail.com>
2021-02-27 21:15:23 +0530

    Setting up Ramda and Vavr dependencies

--- java/src/test/java/InitialTest.java
+++ java/src/test/java/InitialTest.java
@@ -1,8 +1,19 @@
+import io.vavr.control.Try;
 import org.junit.jupiter.api.Test;
 
 public class InitialTest {
     @Test
     void first() {
         System.out.println("true = " + true);
+    }
+
+    @Test
+    void vavrTest() {
+        final int denominator = 2;
+        Integer op1 = Try.of(() -> 10 / denominator).getOrElse(-1);
+        System.out.println("op = " + op1);
+        final int denominatorZero = 0;
+        Integer op2 = Try.of(() -> 10 / denominatorZero).getOrElse(-1);
+        System.out.println("op = " + op2);
     }
 }
--- js/src/first.test.js
+++ js/src/first.test.js
@@ -1,5 +1,13 @@
+const R = require('ramda');
+
 describe("First", () => {
     it("should work", () => {
         console.log("OK");
     })
-})
+    it("Ramda works", () => {
+        const {identity} = R
+        const map = R.map(identity, [1, 2, 3]);
+        console.log(map);
+    });
+});
+
